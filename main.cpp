2845d251b1b75a831c6a7d1dcf54cbfe3c89fbc1
jyroball <jyrojimenez@gmail.com>
2024-11-11 15:04:51 -0800

    added user input for main

--- main.cpp
+++ main.cpp
@@ -7,17 +7,39 @@
 using namespace std;
 
 int main() {
-    
-    
     srand(static_cast<unsigned>(time(nullptr)));
 
+    //user input
     int num_features;
-    cout << "enter the total number of features: ";
+    cout << "Enter the total number of features: ";
     cin >> num_features;
 
+    cout << "\n\nType the number of the algorithm you want to run:\n" 
+         << "(1) Forward Selection\n"  
+         << "(2) Backward Selection\n"
+         << "(3) Group Special Algorithm\n\n";
+
+    int featureSelect;
+    cin >> featureSelect;
+
+    //initialize seleciton class
     Selection selector(num_features);
-    selector.forwardSelection();
-    selector.backwardElimination();
+
+    //output chosen search
+    switch(featureSelect) {
+        case 0:
+            selector.forwardSelection();
+            break;
+        case 1;
+            selector.backwardElimination();
+            break;
+        case 3:
+            //If we wanna add a special Algo for extar credit
+            break;
+        default:
+            selector.forwardSelection();
+            break;
+    }
 
     return 0;
 }
